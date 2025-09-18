c4782504f1871bac482f6080890f75b3fbb8d78a
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-09-18 10:28:48 +0430

    create and export Tip component

---
+++ src/components/Tip.jsx
@@ -0,0 +1,12 @@
+import dollarIcon from "../../public/assets/images/icon-dollar.svg";
+import personIcon from "../../public/assets/images/icon-person.svg";
+
+function Tip() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 bg-white max-w-170 rounded-t-3xl rounded-b-0 sm:rounded-3xl p-7">
+      <div className="flex flex-col gap-8"></div>
+    </div>
+  );
+}
+
+export default Tip;
